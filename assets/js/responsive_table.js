df2edde018d26f7464b7391cc7f29f478d0e6ee5
Ivan Grynenko <2966198+ivangrynenko@users.noreply.github.com>
2026-05-02 11:56:01 +1000

    fix: addressed PR feedback

--- assets/js/responsive_table.js
+++ assets/js/responsive_table.js
@@ -3,15 +3,16 @@
  * JS for wrapping tables in a div with .table-responsive class.
  */
 
-(function ($) {
+(function ($, Drupal, once) {
+
+  'use strict';
 
   Drupal.behaviors.govcms8_uikit_starter_tableResponsive = {
     attach: function (context, settings) {
 
-      var table = $("table");
-      $(table).wrap("<div class='table-responsive'></div>");
+      $(once('govcms8-uikit-starter-table-responsive', 'table', context)).wrap("<div class='table-responsive'></div>");
 
     }
   };
 
-})(jQuery);
+})(jQuery, Drupal, once);
